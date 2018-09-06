4d7ef66e37041a3ca66b257b62286e5d65f139c6
Elizabeth Karst <efkarst@outlook.com>
2018-09-06 01:34:10 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ require_relative './fixtures/kickstarter.html'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  page = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
+
   
+
 end
